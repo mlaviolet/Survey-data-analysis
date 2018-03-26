80a480b7cc7d96be038889077c657ae28edcd548
Michael Laviolette <statman89@comcast.net>
2018-03-26 12:22:40 -0400

    Add files via upload

--- age-adjusted-prevalence.R
+++ age-adjusted-prevalence.R
@@ -1,36 +1,35 @@
-# This script reproduces some results from 
-# "Continuous NHANES Web Tutorial Sample Code & Datasets"
+# reproduce results for tutorial for age-adjusted prevalence from survey data
 # https://www.cdc.gov/nchs/tutorials/NHANES/Downloads/intro.htm#15
+# Age Standardization and Population Counts
+# Task 1a: How to Generate Age-Adjusted Prevalence Rates in SUDAAN
+# Step 1: How to Generate Age-Adjusted Prevalence Rates in SUDAAN
 
-# Using the survey and srvyr packages, reproduce results for tutorial 
-#   "Age Standardization and Population Counts"
-#   Task 1a: How to Generate Age-Adjusted Prevalence Rates in SUDAAN
-#   Step 1: How to Generate Age-Adjusted Prevalence Rates in SUDAAN
-# by Michael Laviolette, PhD, MPH, statman54@gmail.com
+# M. Laviolette
+
 library(tidyverse)
 library(survey)
 library(srvyr)
 # also used: haven
+# setwd("C:/Stat projects/svystandardize")
 
-setwd("My directory")
-# Download data and documentation
-# If download from R fails, download manually using indicated URL's
-# dataset
-download.file("ftp://ftp.cdc.gov/pub/health_statistics/nchs/tutorial/nhanes/Continuous/analysis_data.sas7bdat",
-              "analysis_data.sas7bdat")
-# standard population proportions for NHANES groupings
-download.file("https://www.cdc.gov/nchs/tutorials/nhanes/downloads/Continuous/ageadjwt.xls",
-              "ageadjwt.xls")
-# SAS program to generate age-adjusted prevalence rates
-download.file("ftp://ftp.cdc.gov/pub/health_statistics/nchs/tutorial/nhanes/Continuous/adjprev_sudaan.sas",
-              "adjprev_sudaan.sas")
-# output of SAS program to generate age-adjusted prevalence rates
-download.file("https://www.cdc.gov/nchs/tutorials/nhanes/downloads/Continuous/adjprevalence.pdf",
-              "adjprevalence.pdf")
+setwd("//hzndhhsvf2/data/OCPH/EPI/BHSDM/Group/Michael Laviolette/R programs/BRFSS/svystandardize/NHANES example")
+# # dataset
+# download.file("ftp://ftp.cdc.gov/pub/health_statistics/nchs/tutorial/nhanes/Continuous/analysis_data.sas7bdat",
+#               "analysis_data.sas7bdat")
+# # standard proportions for NHANES population groupings
+# download.file("https://www.cdc.gov/nchs/tutorials/nhanes/downloads/Continuous/ageadjwt.xls",
+#               "ageadjwt.xls")
+# # SAS program to generate age-adjusted prevalence rates
+# download.file("ftp://ftp.cdc.gov/pub/health_statistics/nchs/tutorial/nhanes/Continuous/adjprev_sudaan.sas",
+#               "adjprev_sudaan.sas")
+# # output of program to generate age-adjusted prevalence rates
+# download.file("https://www.cdc.gov/nchs/tutorials/nhanes/downloads/Continuous/adjprevalence.pdf",
+#               "adjprevalence.pdf")
 
 # import from SAS data
-analysis_data <- haven::read_sas("analysis_data.sas7bdat")
+analysis_data <- haven::read_sas("analysis_data.zip")
 
+# computing prevalence of high blood pressure
 # number and mean of non-missing systolic blood pressure entries for 
 #   each respondent
 sbp <- analysis_data %>% 
@@ -39,8 +38,7 @@ sbp <- analysis_data %>%
   group_by(SEQN) %>% 
   summarize(n_sbp = n(),
             mean_sbp = mean(value))
-# confirm that unweighted frequencies match output from SAS PROC FREQ
-sbp %>% count(n_sbp)
+# sbp %>% count(n_sbp)
 
 # number and mean of non-missing diastolic blood pressure entries for 
 #   each respondent
@@ -48,13 +46,14 @@ dbp <- analysis_data %>%
   select(SEQN, starts_with("BPXDI")) %>% 
   gather("key", "value", -SEQN, na.rm = TRUE) %>% 
   # some DBP values are 0; set to missing
-  mutate(value = replace(value, value == 0, NA)) %>% 
+  mutate(value = na_if(value, 0)) %>% 
   # SEQN is unique respondent ID
   group_by(SEQN) %>% 
   summarize(n_dbp = n(),
             mean_dbp = mean(value, na.rm = TRUE))
+# dbp %>% count(n_dbp)  
 
-# recode as per SAS code
+# construct recoded data --------------------------------------------------
 working_data <- 
   # add sbp and dbp to data
   reduce(list(analysis_data, dbp, sbp), inner_join, by = "SEQN") %>% 
@@ -67,114 +66,57 @@ working_data <-
                           RIDRETH1 %in% c(2, 5) ~ "Other",
                           TRUE ~ NA_character_),
          race = factor(race),
-         # reorder factors as "NH-White", "NH-Black", "Mex-Am", "Other"
+         # reorder factors as above
          race = fct_relevel(race, "NH-White", "NH-Black")) %>% 
-  # construct high blood pressure indicator HBP (0 = No, 1 = Yes)
-  # unweighted frequencies match SAS PROC FREQ when rows with n_sbp = 0 (i.e., 
-  #   records with no BP measurements) are removed
+  # construct high blood pressure indicator HBP
+  # frequencies match SAS when rows with n_sbp = 0 are removed (i.e., no 
+  #   BP measurements)
   mutate(HBP_trt = 
            case_when(BPQ050A == 1 ~ 1,
-                     BPQ020 %in% 1:2 & BPQ050A %in% c(2, NA) ~ 0,
-                     TRUE ~ NA_real_)) %>% 
-  mutate(SBP140 = ifelse(mean_sbp >= 140, 1, 0),
-         DBP90 = ifelse(mean_dbp >= 90, 1, 0),
+                     BPQ020 %in% 1:2 & BPQ050A %in% c(2, NA) ~ 0)) %>% 
+  mutate(SBP140 = if_else(mean_sbp >= 140, 1, 0),
+         DBP90 = if_else(mean_dbp >= 90, 1, 0),
          # counting missings for DBP90 matches SAS results
          DBP90 = replace(DBP90, DBP90 %in% c(0, NA), 0)) %>% 
-  mutate(HBP = case_when(HBP_trt == 0 & SBP140 == 0 & DBP90 == 0 ~ 2, 
-                         HBP_trt %in% 0:1 ~ 1,
-                         # TRUE ~ as.numeric(HBP_trt)
-                         TRUE ~ NA_real_),
-         HBP = factor(HBP, 1:2, c("Yes", "No")))
+  mutate(HBP = case_when(HBP_trt == 0 & SBP140 == 0 & DBP90 == 0 ~ "No", 
+                         HBP_trt %in% 0:1 ~ "Yes",
+                         TRUE ~ NA_character_),
+         HBP = factor(HBP))
 rm(sbp, dbp, analysis_data)
+# check--looks good
+lapply(working_data[c("RIAGENDR", "age", "race", "HBP")], summary)
+lapply(working_data[c("n_sbp", "n_dbp", "HBP_trt", "SBP140", "DBP90")], 
+       table, exclude = NULL)
 
-# check against SAS 
-lapply(working_data[c("RIAGENDR", "age", "race")], summary)
-
-# age       Frequency
-# ------------------
-#     .         5996
-# 20-39         3238
-# 40-59         2711
-# 60+           3107
-# 
-# race       Frequency
-# --------------------
-# NH-White        5954
-# NH-Black        3472
-# Mex-Am          4348
-# Other           1278
-#  
-# RIAGENDR    Frequency
-# ---------------------
-#   Male           7309
-#   Female         7743
-
-lapply(working_data[c("n_sbp", "n_dbp", "HBP_trt", "SBP140", "DBP90", 
-                      "HBP")], table, exclude = NULL)
-# n_sbp    Frequency
-# ------------------
-#   1         413
-#   2        1173
-#   3       13466
-# 
-# n_dbp    Frequency
-# ------------------
-#   1         413
-#   2        1174
-#   3       13465
-# 
-# HBP_trt    Frequency
-# ------------------
-#   .        3967
-#   0        9162
-#   1        1923
-# 
-# SBP140    Frequency
-# ------------------
-#   0       12978
-#   1        2074
-# 
-# DBP90    Frequency
-# ------------------
-#   0       14471
-#   1         581
-
-# HBP    Frequency
-# ------------------
-#   .        3967
-#   1        3201
-#   2        7884
-
-# create initial survey object
+# age-adjusted prevalence -------------------------------------------------
+# standard population, age groups 20-39, 40-59, 60+
+std_pop <- c(77670, 72816, 45364)
+# unadjusted survey object 
 svy_crude <- working_data %>% 
   as_survey_design(ids = SDMVPSU, strata = SDMVSTRA, weights = WTMEC4YR, 
                    nest = TRUE)
-
 # for each domain, construct adjusted survey object and obtain prevalence
-# standard population for age groups 20-39, 40-59, 60+
-std_pop <- c(77670, 72816, 45364)
 
 # Population age 20 and older
-# Age-adjusted prevalence of high blood pressure per SUDAAN
 # --------------------------------
 #        Sample           Standard
 #        Size   Percent   Error
 # --------------------------------
 # Total  8960   29.3433   0.8381
 
-# since adjusting over entire survey, need a variable with the same
-#   value for all records--since not present, creating on the fly with
+# sink("age-adj.txt")
+# since adjusting over entire dataset, need a variable with the same
+#   value throughout--since not present, creating on the fly with
 #   update function
-svystandardize(update(svy_crude, dummy = 1), by = ~ age, over = ~ dummy,
+svystandardize(update(svy_crude, dummy = 1), by = ~age, over = ~dummy,
                population = std_pop,
-               excluding.missing = ~ age + HBP) %>%
+               excluding.missing = ~age + HBP) %>%
   filter(RIDAGEYR >= 20) %>% 
   summarize(n = unweighted(n()),
     pct = survey_mean(HBP == "Yes")) %>% 
   mutate_at(vars(starts_with("pct")), function(x) 100 * x)
 
 # by gender
-# SUDAAN results
 # ----------------------------------
 #         Sample           Standard
 # Gender  Size   Percent   Error
@@ -182,9 +124,9 @@ svystandardize(update(svy_crude, dummy = 1), by = ~ age, over = ~ dummy,
 # Male    4228   28.3277   1.2083
 # Female  4732   29.9706   0.7126
 
-svystandardize(svy_crude, by = ~ age, over = ~ RIAGENDR,
+svystandardize(svy_crude, by = ~age, over = ~RIAGENDR,
                population = std_pop,
-               excluding.missing = ~ age + RIAGENDR + HBP) %>% 
+               excluding.missing = ~age + RIAGENDR + HBP) %>% 
   filter(RIDAGEYR >= 20) %>% 
   group_by(RIAGENDR) %>% 
   summarize(n = unweighted(n()),
@@ -192,7 +134,6 @@ svystandardize(svy_crude, by = ~ age, over = ~ RIAGENDR,
   mutate_at(vars(starts_with("pct")), function(x) 100 * x)
 
 # by race
-# SUDAAN results
 # -----------------------------------------------------
 #                 Sample            Standard
 # Race-Ethnicity  Size    Percent   Error
@@ -202,9 +143,9 @@ svystandardize(svy_crude, by = ~ age, over = ~ RIAGENDR,
 # Mex-Am          2087    26.0541   0.9851
 # Other            757    30.5625   2.2547
 
-svystandardize(svy_crude, by = ~ age, over = ~ race,
+svystandardize(svy_crude, by = ~age, over = ~race,
                population = std_pop,
-               excluding.missing = ~ age + race + HBP) %>% 
+               excluding.missing = ~age + race + HBP) %>% 
   filter(RIDAGEYR >= 20) %>% 
   group_by(race) %>% 
   summarize(n = unweighted(n()),
@@ -212,7 +153,6 @@ svystandardize(svy_crude, by = ~ age, over = ~ race,
   mutate_at(vars(starts_with("pct")), function(x) 100 * x)
 
 # by race and gender
-# SUDAAN results
 # -----------------------------------------------------
 #                        Sample           Standard
 # Gender  Race-Ethnicity Size   Percent   Error
@@ -226,14 +166,40 @@ svystandardize(svy_crude, by = ~ age, over = ~ race,
 # Female  Mex-Am         1130   25.9149   1.1827
 # Female  Other           429   32.0618   2.4540
 
-svystandardize(svy_crude, by = ~ age, over = ~ RIAGENDR + race,
+svystandardize(svy_crude, by = ~age, over = ~RIAGENDR + race,
                population = std_pop,
-               excluding.missing = ~ age + RIAGENDR + race + HBP) %>% 
+               excluding.missing = ~age + race + RIAGENDR + HBP) %>% 
   filter(RIDAGEYR >= 20) %>% 
   group_by(RIAGENDR, race) %>% 
   summarize(n = unweighted(n()),
             pct = survey_mean(HBP == "Yes")) %>% 
   mutate_at(vars(starts_with("pct")), function(x) 100 * x)
 
-# next revision will include function to compute prevalence
+# sink()
+
+
+
+# # estimate of high BP in population age 20 or older
+# svy_crude %>% 
+#   filter(RIDAGEYR >= 20 & !is.na(HBP)) %>% 
+#   summarize(n = unweighted(n()),
+#             pct = survey_mean(HBP == "Yes", na.rm = TRUE))
+# 
+# # by gender
+# svy_crude %>% 
+#   filter(RIDAGEYR >= 20) %>% 
+#   group_by(RIAGENDR) %>% 
+#   summarize(pct = survey_mean(HBP == "Yes", na.rm = TRUE))
+# 
+# # by race
+# svy_crude %>% 
+#   filter(RIDAGEYR >= 20) %>% 
+#   group_by(race) %>% 
+#   summarize(pct = survey_mean(HBP == "Yes", na.rm = TRUE))
+# 
+# # by gender and race
+# svy_crude %>% 
+#   filter(RIDAGEYR >= 20) %>% 
+#   group_by(RIAGENDR, race) %>% 
+#   summarize(pct = survey_mean(HBP == "Yes", na.rm = TRUE))
 
